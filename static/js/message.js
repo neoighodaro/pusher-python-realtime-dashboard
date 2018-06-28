9fc777d980055bf55a5d80652eb35f9d4a6fd5b4
Neo Ighodaro <neo@creativitykills.co>
2018-06-28 12:48:35 +0100

    Update message.js

--- static/js/message.js
+++ static/js/message.js
@@ -1,23 +1,18 @@
-(function($) {
-    "use strict"; // Start of use strict
+$(document).ready(function () {
+  (function($) {
+    "use strict";
+
     // Configure tooltips for collapsed side navigation
     $('.navbar-sidenav [data-toggle="tooltip"]').tooltip({
       template: '<div class="tooltip navbar-sidenav-tooltip" role="tooltip" style="pointer-events: none;"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
     })
+
     // Configure tooltips globally
     $('[data-toggle="tooltip"]').tooltip()
-    // Smooth scrolling using jQuery easing
-    $(document).on('click', 'a.scroll-to-top', function(event) {
-      var $anchor = $(this);
-      $('html, body').stop().animate({
-        scrollTop: ($($anchor.attr('href')).offset().top)
-      }, 1000, 'easeInOutExpo');
-      event.preventDefault();
-    });
-  })(jQuery); // End of use strict
-  
+  })(jQuery);
+
   var messageChannel = pusher.subscribe('message');
-  
+
   // Listen to 'message sent' event
   messageChannel.bind('send', function(data) {
     var message = document.getElementById('message-count')
@@ -36,3 +31,4 @@
                   '</div>'
     message.innerText = parseInt(message.innerText)+1
   });
+});
